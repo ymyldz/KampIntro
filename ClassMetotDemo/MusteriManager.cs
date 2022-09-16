a2351021c7d9adde5ffbf7cb3b07eb2435ad94e5
YUSUF MERT YILDIZ <yldzymy1905@gmail.com>
2022-09-17 01:31:20 +0300

    .

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+     class MusteriManager
+    {
+        public void Ekle(Musteri m) 
+        {
+            Console.WriteLine("Müşteri eklendi : "  + m.Adi);
+
+       
+            
+        
+        }
+
+   
+        public void Sil(Musteri m) 
+        {
+            Console.WriteLine("Müşteri silindi : " + m.Adi);
+      
+        }
+        public void Listele(Musteri m) 
+        {
+            Console.WriteLine("Müşteriler listelendi : " + 
+                "Müşteri Adı : "+m.Adi+
+                "Müşteri Soyadı : " + m.Soyadi+
+                "Müşteri Yaşı : "+m.Yas+ 
+                "Müşteri Puanı"+ m.krediPuani);
+        }
+
+    }
+}
