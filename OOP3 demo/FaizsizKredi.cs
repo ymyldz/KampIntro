10f7126b33701ceef50a7a8dcb93665a0de6bf0e
YUSUF MERT YILDIZ <yldzymy1905@gmail.com>
2022-09-24 11:37:49 +0300

    the muslim of bank :D

---
+++ OOP3 demo/FaizsizKredi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3_demo
+{
+    internal class FaizsizKredi : IKrediManager
+    {
+        public void Hesapla()
+        {
+            Console.WriteLine("Faizsiz kredi hesaplandı");
+        }
+
+        public void Listele()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OOP3 demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using OOP3_demo;
+
+IKrediManager sifirfaizlikredi = new SifirFaizliKredi();
+IKrediManager hicfaizsizkredi = new HicFaizsizKredi();
+IKrediManager faizsizkredi=new FaizsizKredi();
+ILoggerService smslog = new SmsLog();
+ILoggerService filelog = new FileLog();
+
+sifirfaizlikredi.Hesapla();
+smslog.Log();
+
+
+List<IKrediManager> krediler = new List<IKrediManager>() {sifirfaizlikredi,hicfaizsizkredi,faizsizkredi };
+List<ILoggerService> loggers = new List<ILoggerService>() { smslog, filelog };
+
+BasvuruManager basvuruManager=new BasvuruManager();
+basvuruManager.BasvuruYap(faizsizkredi, smslog);
+
+sifirfaizlikredi.Hesapla();
+
+
